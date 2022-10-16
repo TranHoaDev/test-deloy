a15c4be1493450022cf1f59086ac82aaf7eb2279
TranHoaDev <tranhoaksb104@gmail.com>
2022-10-16 20:11:54 +0700

    Update v1

--- src/main/resources/db/migration/V4__Add_orders.sql
+++ src/main/resources/db/migration/V4__Add_orders.sql
@@ -1,13 +1,13 @@
 INSERT INTO orders (id, address, city, date, email, first_name, last_name, phone_number, post_index, total_price)
-    VALUES (1, 'Wall Street1', 'New York', '2021-04-07', 'test123@test.com', 'John', 'Doe', '1234567890', 1234567890, 840);
+    VALUES (1, 'f14, Tan Binh', 'Ho Chi Minh', '2022-10-04', 'test123@test.com', 'Tran', 'Hoa', '0123456789', 56789, 840);
 INSERT INTO orders (id, address, city, date, email, first_name, last_name, phone_number, post_index, total_price)
-    VALUES (2, 'Wall Street1', 'New York', '2021-04-07', 'test123@test.com', 'John', 'Doe', '1234567890', 1234567890, 240);
+    VALUES (2, 'f14, Tan Binh', 'Ho Chi Minh', '2022-10-01', 'test123@test.com', 'Tran', 'Hoa', '0123456789', 56789, 240);
 INSERT INTO orders (id, address, city, date, email, first_name, last_name, phone_number, post_index, total_price)
-    VALUES (3, 'Tverskaya street 1', 'Moscow', '2021-04-07', 'ivan123@test.com', 'Ivan', 'Ivanov', '1234567890', 1234567890, 163);
+    VALUES (3, 'Kim Son', 'Ninh Binh', '2022-10-02', 'demo123@test.com', 'Minh', 'Le', '1234567890', 12345, 163);
 INSERT INTO orders (id, address, city, date, email, first_name, last_name, phone_number, post_index, total_price)
-    VALUES (4, 'Tverskaya street 1', 'Moscow', '2021-04-07', 'ivan123@test.com', 'Ivan', 'Ivanov', '1234567890', 1234567890, 780);
+    VALUES (4, 'Kim Son', 'Ninh Binh', '2022-10-03', 'demo123@test.com', 'Minh', 'Le', '1234567890', 12345, 780);
 INSERT INTO orders (id, address, city, date, email, first_name, last_name, phone_number, post_index, total_price)
-    VALUES (5, 'Tverskaya street 1', 'Moscow', '2021-04-07', 'ivan123@test.com', 'Ivan', 'Ivanov', '1234567890', 1234567890, 196);
+    VALUES (5, 'Kim Son', 'Ninh Binh', '2022-10-07', 'demo123@test.com', 'Minh', 'Le', '1234567890', 12345, 196);
 
 INSERT INTO order_item (id, amount, quantity, perfume_id) VALUES (1, 384, 2, 33);
 INSERT INTO order_item (id, amount, quantity, perfume_id) VALUES (2, 456, 3, 34);
