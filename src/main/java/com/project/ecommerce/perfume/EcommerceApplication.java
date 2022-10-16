134d589a0fe4163e3d4d75a9476c21f4f1827ac8
TranHoaDev <tranhoaksb104@gmail.com>
2022-10-16 23:06:50 +0700

    Update v8

--- src/main/java/com/project/ecommerce/perfume/EcommerceApplication.java
+++ src/main/java/com/project/ecommerce/perfume/EcommerceApplication.java
@@ -2,6 +2,10 @@ package com.project.ecommerce.perfume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class EcommerceApplication {
@@ -10,4 +14,13 @@ public class EcommerceApplication {
         SpringApplication.run(EcommerceApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("https://ecommerce-fe-three.vercel.app");
+            }
+        };
+    }
 }
